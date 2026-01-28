6ec0242665e4b3c9e3215858d43afa7831a7de8e
nalyo <1300547570@qq.com>
2026-01-28 19:17:39 +0800

    fix db.h

--- IM/src/db/db.h
+++ IM/src/db/db.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <sqlite3.h>
 
-int db_init(const char* db_name, const char* sqls[]);
+int db_init(const char* db_name, const char* sqls[], int sql_count);
 void db_close(void);
 
 sqlite3* db_get(void);
